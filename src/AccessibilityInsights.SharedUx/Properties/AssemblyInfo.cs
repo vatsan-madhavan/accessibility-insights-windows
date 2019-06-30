d1215183cdf3de50b557a1911f98bc834c1a2e22
Vatsan Madhavan <vatsan.madhavan@gmail.com>
2019-06-30 11:52:29 -0700

    Convert ShardUx toSdk style project

--- src/AccessibilityInsights.SharedUx/Properties/AssemblyInfo.cs
+++ src/AccessibilityInsights.SharedUx/Properties/AssemblyInfo.cs
@@ -1,22 +1,10 @@
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("AccessibilityInsights.SharedUx")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AccessibilityInsights.SharedUx")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -31,7 +19,6 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-[assembly: NeutralResourcesLanguage("en-US")]
 
 
 #if ENABLE_SIGNING
