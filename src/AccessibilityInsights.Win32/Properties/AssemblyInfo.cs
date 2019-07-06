a705fbaf56d53b7ff391e6697603128144d382ff
Vatsan Madhavan <vatsan.madhavan@gmail.com>
2019-07-06 14:45:08 -0700

    Convert Win32 to sdk style project

--- src/AccessibilityInsights.Win32/Properties/AssemblyInfo.cs
+++ src/AccessibilityInsights.Win32/Properties/AssemblyInfo.cs
@@ -1,21 +1,9 @@
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("AccessibilityInsights.Win32")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AccessibilityInsights.Win32")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
